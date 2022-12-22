6a820d990ddb36935772e975fecb333335880b51
Chris McCauley <cdmccauley@gmail.com>
2022-12-21 23:00:00 -0700

    20 seconds

--- data/use-orders.js
+++ data/use-orders.js
@@ -7,7 +7,7 @@ export default function useOrders(props) {
   const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
   const { data, error } = useSWR(nftId ? url : null, fetcher, {
-    refreshInterval: 5 * 1000, //60000 * 5, //
+    refreshInterval: 20 * 1000, //60000 * 5, //
     refreshWhenHidden: true,
     revalidateOnFocus: false
   });
